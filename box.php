653b52956e870929dc4dbce68d8b82b2208bedf6
Milamber <milamber@gmail.com>
2013-08-07 11:04:47 -0400

    Updated to be compatible with Thesis 2.1

--- box.php
+++ box.php
@@ -1,64 +1,79 @@
 <?php
 /*
 Name: PHP Box
-Author: Tao Schencks, Modified by Tim Milligan
-Version: 1.2
-Description: A Thesis 2 Box That Accepts PHP code
-Class: thesis_php_box
+Author: Tim Milligan
+Version: 2.1
+Description: An extension of the Thesis Text Box that supports PHP code.
+Class: vzm_php_box
 */
 
-
-class thesis_php_box extends thesis_box {
-	
+class vzm_php_box extends thesis_box {
 	protected function translate() {
-		$this->title = $this->name = __('PHP Box', 'tpb');
+		$this->title = $this->name = __('PHP Box', $this->_class);
 	}
-	
+
+	protected function construct() {
+		global $thesis;
+		$filters = !empty($this->options['filter']['on']) ?
+			array(
+				'wptexturize' => false,
+				'convert_smilies' => false,
+				'convert_chars' => false,
+				'wpautop' => false,
+				'shortcode_unautop' => false,
+				'do_shortcode' => false) :
+			array(
+				'wptexturize' => false,
+				'convert_smilies' => false,
+				'convert_chars' => false,
+				'do_shortcode' => false);
+		$thesis->wp->filter($this->_id, $filters);
+	}
+
+	protected function html_options() {
+		global $thesis;
+		$html = $thesis->api->html_options(array(
+			'div' => 'div',
+			'none' => sprintf(__('No %s wrapper', $this->_class), $thesis->api->base['html'])), 'div');
+		$html['html']['dependents'] = array('div');
+		$html['id']['parent'] = $html['class']['parent'] = array('html' => 'div');
+		return $html;
+	}
+
 	protected function options() {
 		global $thesis;
 		return array(
-			'code' => array(
+			'text' => array(
 				'type' => 'textarea',
 				'rows' => 8,
 				'code' => true,
-				'label' => __('PHP code', 'tpb'),
-				'tooltip' => sprintf(__('Enter your PHP code here, including the opening and closing tags', 'tpb')),
-				'default' => ''
-				),
-			'id' => array(
-				'type' => 'text',
-				'width' => 'medium',
-				'code' => true,
-				'label' => $thesis->api->strings['html_id'],
-				'tooltip' => $thesis->api->strings['id_tooltip']),
-			'class' => array(
-				'type' => 'text',
-				'width' => 'medium',
-				'code' => true,
-				'label' => $thesis->api->strings['html_class'],
-				'tooltip' => $thesis->api->strings['class_tooltip'])
-			);
+				'label' => sprintf(__('%s/%s', $this->_class), $thesis->api->base['php'], $thesis->api->base['html']),
+				'tooltip' => sprintf(__('This box allows you to insert plain text and/or %2$s/%1$s. All text will be formatted just like a normal WordPress post, and all valid %1$s tags are allowed.', $this->_class), $thesis->api->base['html'], $thesis->api->base['php'])),
+			'filter' => array(
+				'type' => 'checkbox',
+				'options' => array(
+					'on' => __('enable automatic <code>&lt;p&gt;</code> tags for this PHP Box', $this->_class))));
 	}
 
-	public function html($args = false) {
+	public function html($args = array()) {
 		global $thesis;
 		extract($args = is_array($args) ? $args : array());
 		$tab = str_repeat("\t", !empty($depth) ? $depth : 0);
-		
-		if(empty($this->options['code'])) return;
-		
-		$code = $this->eval_php(stripslashes($this->options['code']));		
-		
-		echo "$tab<div" . ($this->options['id'] ? ' id="' . trim($thesis->api->esc($this->options['id'])) . '"' : '') . ($this->options['class'] ? ' class="' . trim($thesis->api->esc($this->options['class'])) . '"' : '') . ">\n";
-		echo "$tab\t".trim($code)."\n";
-        echo "$tab</div>\n";
+		$html = !empty($this->options['html']) ? ($this->options['html'] == 'none' ? false : $this->options['html']) : 'div';
+		if (empty($this->options['text']) && !is_user_logged_in()) return;
+		echo
+			($html ?
+			"$tab<div" . (!empty($this->options['id']) ? ' id="' . trim($thesis->api->esc($this->options['id'])) . '"' : '') . ' class="' . (!empty($this->options['class']) ? trim($thesis->api->esc($this->options['class'])) : 'text_box') . "\">\n" : ''),
+			$tab, ($html ? "\t" : ''), trim(apply_filters($this->_id, !empty($this->options['text']) ?
+				$this->eval_php(stripslashes($this->options['text'])) :
+				sprintf(__('This is a Text Box named %1$s. You can write anything you want in here, and Thesis will format it just like a WordPress post. <a href="%2$s">Click here to edit this Text Box</a>.', $this->_class), $this->name, admin_url("admin.php?page=thesis&canvas=$this->_id")))), "\n",
+			($html ?
+			"$tab</div>\n" : '');
 	}
 	
-	public function eval_php($content) {
+	protected function eval_php($content) {
 		ob_start();
 			eval("?>$content<?php ");
 		return ob_get_clean();
 	}
 }
-
-?>
